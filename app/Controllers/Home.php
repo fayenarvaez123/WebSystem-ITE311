96f37adafa19de09bd78839bc6c0f3c2d2f045de
Your New Name <your-email@example.com>
2025-08-21 12:37:18 +0800

    Added Home controller, routes, and views for basic navigation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,19 +4,19 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    // Homepage
+    // Homepage area
     public function index(): string
     {
         return view('home');  // loads app/Views/home.php
     }
 
-    // About Page
+    // About Page area
     public function about(): string
     {
         return view('about'); // loads app/Views/about.php
     }
 
-    // Contact Page
+    // Contact Page area
     public function contact(): string
     {
         return view('contact'); // loads app/Views/contact.php
