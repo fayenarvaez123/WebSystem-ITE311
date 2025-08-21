4389772ae4e21e650d5482b35b1bb9d86fa43206
Your New Name <your-email@example.com>
2025-08-21 12:26:50 +0800

    Added Bootstrap template and Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,21 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    // Homepage
     public function index(): string
     {
-        return view('welcome_message');
+        return view('home');  // loads app/Views/home.php
+    }
+
+    // About Page
+    public function about(): string
+    {
+        return view('about'); // loads app/Views/about.php
+    }
+
+    // Contact Page
+    public function contact(): string
+    {
+        return view('contact'); // loads app/Views/contact.php
     }
 }
